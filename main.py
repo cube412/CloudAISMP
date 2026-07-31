4e704b5486a3fce603d3eaffd5b90435da2c610c
cube412 <hehehedz215@gmail.com>
2026-07-31 14:31:22 +0700

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import os
+import discord
+import google.generativeai as genai
+from dotenv import load_dotenv
 
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
