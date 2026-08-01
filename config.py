2895bffcd0eebab2e9ee89204977d795a639e424
cube412 <hehehedz215@gmail.com>
2026-08-01 19:46:45 +0700

    Update Discord bot settings and AI model version
    Added configuration for Discord bot and updated AI model.

--- config.py
+++ config.py
@@ -3,13 +3,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# =========================
+# Discord
+# =========================
+
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
-BOT_NAME = "CloudAI"
-
-AI_MODEL = "gemini-flash-latest"
 
 PREFIX = "!"
 
+BOT_NAME = "CloudAI"
+
+# Discord User ID của Owner
+ADMIN_ID = 1514447473748475975
+
+# =========================
+# Gemini AI
+# =========================
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+AI_MODEL = "gemini-3.6-flash"
+
 MAX_HISTORY = 10
+
+# =========================
+# CloudAI
+# =========================
+
+VERSION = "5.0"
+
+EMBED_COLOR = 0x00BFFF
